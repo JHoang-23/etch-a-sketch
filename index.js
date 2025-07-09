d7195cbc69f3ce93edef354c6c0d4b958ac67e04
John <johnhoang239@gmail.com>
2025-07-09 15:59:39 +0100

    Add grid creation and resize functionality
    - Added prompt to set number of squares per side (max 100)
    - Created grid based on user input, defaulting to 16x16
    - Highlight grid squares on mouseover

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const container = document.querySelector(".container")
+const btn = document.querySelector(".btn")
 
-//Create 16 * 16 div sqaures.
-for (let i = 0; i < 16*16; i++){
-    const newDiv = document.createElement('div')
-    newDiv.style.height = "40px"
-    newDiv.style.width = "40px"
-    newDiv.style.border = "1px solid black";
-    container.append(newDiv)
 
+
+container.addEventListener("mouseover", e =>
+    e.target.classList.add("highlight"))
+
+btn.addEventListener("click", function() {
+    let squaresPerSide = parseInt(prompt("How many squares per side?"))
+    while(squaresPerSide > 100){
+        squaresPerSide = parseInt(prompt(`How many squares per side?
+            \nMust be less than 100`))
+            
+    }
+    createGrid(squaresPerSide)
+})
+
+function calculateSizing(squaresPerSide){
+    //grid is currently 640x640
+    return (640/squaresPerSide)
 }
+
+function createGrid(squaresPerSide = 16){
+    console.log(`Current size: ${squaresPerSide}`)
+    //Create 16 * 16 div sqaures.
+    container.innerHTML = ''
+    for (let i = 0; i < squaresPerSide**2; i++){
+        const newDiv = document.createElement('div')
+        newDiv.style.height = `${calculateSizing(squaresPerSide)}px`
+        newDiv.style.width = `${calculateSizing(squaresPerSide)}px`
+        newDiv.style.border = "1px solid black";
+        container.append(newDiv)
+
+    }
+}
+
+createGrid()
